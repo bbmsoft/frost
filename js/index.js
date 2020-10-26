f5b73861eee525446d14c6bade9c56fc69917d3e
Michael Bachmann <mbachmann@bbmsoft.net>
2020-10-26 08:31:09 +0100

    streamlining init sequence

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
-import("../pkg/index.js").catch(console.error);
-
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/service-worker.js').then(registration => {
             console.log('SW registered: ', registration);
+            loadWasm();
         }).catch(registrationError => {
             console.log('SW registration failed: ', registrationError);
+            loadWasm();
         });
     });
 } else {
     console.error("ServiceWorker not available!");
+    loadWasm();
+}
+
+function loadWasm() {
+    import("../pkg/index.js").catch(console.error);
 }
--- pack-local.sh
+++ pack-local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wasm-pack build --target web --dev
 cp -r static/* pkg
-echo "function placesInitialized() {import('/frost.js').then(async wasm => await wasm.default());}" >pkg/index.js
+echo "import('/frost.js').then(async wasm => await wasm.default());" >pkg/index.js
